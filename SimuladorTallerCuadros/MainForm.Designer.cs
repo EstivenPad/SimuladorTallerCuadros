b1934aba427bec44da7a79b830da89a9d117eb60
Jose Armando Flores Baldera <jose_flores3@ucne.edu.do>
2021-03-24 20:37:16 -0400

    Casi teiminao

--- SimuladorTallerCuadros/MainForm.Designer.cs
+++ SimuladorTallerCuadros/MainForm.Designer.cs
@@ -34,7 +34,6 @@
             this.label1 = new System.Windows.Forms.Label();
             this.TiempoTextBox = new System.Windows.Forms.TextBox();
             this.CronometroTimer = new System.Windows.Forms.Timer(this.components);
-            this.button1 = new System.Windows.Forms.Button();
             this.Carpintero1Timer = new System.Windows.Forms.Timer(this.components);
             this.Carpintero2Timer = new System.Windows.Forms.Timer(this.components);
             this.Carpintero3Timer = new System.Windows.Forms.Timer(this.components);
@@ -42,101 +41,125 @@
             this.Carpintero5Timer = new System.Windows.Forms.Timer(this.components);
             this.TiempoLabel = new System.Windows.Forms.Label();
             this.LlegadaTimer = new System.Windows.Forms.Timer(this.components);
+            this.ProcesosTimer = new System.Windows.Forms.Timer(this.components);
+            this.IniciarButton = new System.Windows.Forms.Button();
+            this.PintarTimer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // CantidadMarcosTextBox
             // 
-            this.CantidadMarcosTextBox.Location = new System.Drawing.Point(462, 156);
+            this.CantidadMarcosTextBox.Location = new System.Drawing.Point(616, 192);
+            this.CantidadMarcosTextBox.Margin = new System.Windows.Forms.Padding(4);
             this.CantidadMarcosTextBox.Name = "CantidadMarcosTextBox";
-            this.CantidadMarcosTextBox.Size = new System.Drawing.Size(200, 20);
+            this.CantidadMarcosTextBox.Size = new System.Drawing.Size(265, 22);
             this.CantidadMarcosTextBox.TabIndex = 0;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(459, 109);
+            this.label2.Location = new System.Drawing.Point(612, 134);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.Size = new System.Drawing.Size(134, 17);
             this.label2.TabIndex = 3;
             this.label2.Text = "Cantidad de Marcos";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(85, 109);
+            this.label1.Location = new System.Drawing.Point(113, 134);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.Size = new System.Drawing.Size(55, 17);
             this.label1.TabIndex = 2;
             this.label1.Text = "Tiempo";
             // 
             // TiempoTextBox
             // 
-            this.TiempoTextBox.Location = new System.Drawing.Point(88, 156);
+            this.TiempoTextBox.Location = new System.Drawing.Point(117, 192);
+            this.TiempoTextBox.Margin = new System.Windows.Forms.Padding(4);
             this.TiempoTextBox.Name = "TiempoTextBox";
-            this.TiempoTextBox.Size = new System.Drawing.Size(200, 20);
+            this.TiempoTextBox.Size = new System.Drawing.Size(265, 22);
             this.TiempoTextBox.TabIndex = 1;
             // 
             // CronometroTimer
             // 
-            this.CronometroTimer.Interval = 500;
+            this.CronometroTimer.Interval = 50;
             this.CronometroTimer.Tick += new System.EventHandler(this.CronometroTimer_Tick);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(287, 233);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(160, 72);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Carpintero1Timer
             // 
+            this.Carpintero1Timer.Interval = 50;
             this.Carpintero1Timer.Tick += new System.EventHandler(this.Carpintero1Timer_Tick);
             // 
             // Carpintero2Timer
             // 
+            this.Carpintero2Timer.Interval = 50;
             this.Carpintero2Timer.Tick += new System.EventHandler(this.Carpintero2Timer_Tick);
             // 
             // Carpintero3Timer
             // 
+            this.Carpintero3Timer.Interval = 50;
             this.Carpintero3Timer.Tick += new System.EventHandler(this.Carpintero3Timer_Tick);
             // 
             // Carpintero4Timer
             // 
+            this.Carpintero4Timer.Interval = 50;
             this.Carpintero4Timer.Tick += new System.EventHandler(this.Carpintero4Timer_Tick);
             // 
             // Carpintero5Timer
             // 
+            this.Carpintero5Timer.Interval = 50;
             this.Carpintero5Timer.Tick += new System.EventHandler(this.Carpintero5Timer_Tick);
             // 
             // TiempoLabel
             // 
             this.TiempoLabel.AutoSize = true;
             this.TiempoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TiempoLabel.Location = new System.Drawing.Point(336, 156);
+            this.TiempoLabel.Location = new System.Drawing.Point(448, 192);
+            this.TiempoLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.TiempoLabel.Name = "TiempoLabel";
-            this.TiempoLabel.Size = new System.Drawing.Size(54, 20);
+            this.TiempoLabel.Size = new System.Drawing.Size(67, 25);
             this.TiempoLabel.TabIndex = 5;
             this.TiempoLabel.Text = "00:00";
             // 
             // LlegadaTimer
             // 
-            this.LlegadaTimer.Interval = 500;
+            this.LlegadaTimer.Interval = 50;
             this.LlegadaTimer.Tick += new System.EventHandler(this.LlegadaTimer_Tick);
+            // 
+            // ProcesosTimer
+            // 
+            this.ProcesosTimer.Interval = 50;
+            this.ProcesosTimer.Tick += new System.EventHandler(this.ProcesosTimer_Tick);
+            // 
+            // IniciarButton
+            // 
+            this.IniciarButton.Location = new System.Drawing.Point(494, 319);
+            this.IniciarButton.Name = "IniciarButton";
+            this.IniciarButton.Size = new System.Drawing.Size(75, 35);
+            this.IniciarButton.TabIndex = 6;
+            this.IniciarButton.Text = "button1";
+            this.IniciarButton.UseVisualStyleBackColor = true;
+            this.IniciarButton.Click += new System.EventHandler(this.IniciarButton_Click);
+            // 
+            // PintarTimer
+            // 
+            this.PintarTimer.Interval = 50;
+            this.PintarTimer.Tick += new System.EventHandler(this.PintarTimer_Tick);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.IniciarButton);
             this.Controls.Add(this.TiempoLabel);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.TiempoTextBox);
             this.Controls.Add(this.CantidadMarcosTextBox);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainForm";
             this.Text = "Simulador";
             this.Load += new System.EventHandler(this.MainForm_Load);
@@ -151,7 +174,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox TiempoTextBox;
         private System.Windows.Forms.Timer CronometroTimer;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer Carpintero1Timer;
         private System.Windows.Forms.Timer Carpintero2Timer;
         private System.Windows.Forms.Timer Carpintero3Timer;
@@ -159,6 +181,9 @@
         private System.Windows.Forms.Timer Carpintero5Timer;
         private System.Windows.Forms.Label TiempoLabel;
         private System.Windows.Forms.Timer LlegadaTimer;
+        private System.Windows.Forms.Timer ProcesosTimer;
+        private System.Windows.Forms.Button IniciarButton;
+        private System.Windows.Forms.Timer PintarTimer;
     }
 }
 
--- SimuladorTallerCuadros/MainForm.cs
+++ SimuladorTallerCuadros/MainForm.cs
@@ -17,6 +17,9 @@ namespace SimuladorTallerCuadros
 
         public Queue<int> ColaDeLlegada = new Queue<int>();
         public Queue<int> ColaDeEnsamblaje = new Queue<int>();
+        public Queue<int> ColaDeAlmacen = new Queue<int>();
+        public Queue<int> ColaPintado = new Queue<int>();
+        public Queue<int> ColaEmpaque = new Queue<int>();
 
         public bool Carpintero1Disponible = true;
         public bool Carpintero2Disponible = true;
@@ -30,9 +33,32 @@ namespace SimuladorTallerCuadros
         public int TiempoCarpintero4;
         public int TiempoCarpintero5;
 
-        public static int hora = 0;
-        public static int minuto = 0;
-        public static int segundo = 0;
+        public int HoraInicioC1;
+        public int HoraInicioC2;
+        public int HoraInicioC3;
+        public int HoraInicioC4;
+        public int HoraInicioC5;
+
+        public static int Dia = 0;
+        public static int Hora = 0;
+        public static int Minuto = 0;
+
+        public static int InicioPintado = 0;
+        public static int DuracionPintado = 0;
+        public static int TerminarPintado = 0;
+
+        public static int InicioEmpaque = 0;
+        public static int DuracionEmpaque = 0;
+        public static int TerminarEmpaque = 0;
+
+        public static int InicioMantenimiento = 0;
+        public static int DuracionMantenimiento = 0;
+        public static int TerminarMantenimiento = 0;
+
+        public static int MarcosProcesados = 0;
+
+        public bool MarcoPintado = false;
+        public bool MarcoEmpacado = false;
 
         public MainForm()
         {
@@ -66,6 +92,7 @@ namespace SimuladorTallerCuadros
             }
 
             Ensamblar();
+            LlegadaTimer.Interval = 15000;
         }
 
         public void Ensamblar()
@@ -78,53 +105,173 @@ namespace SimuladorTallerCuadros
             TiempoCarpintero4 = aleatorio.Next(2, 7);
             TiempoCarpintero5 = aleatorio.Next(2, 7);
 
-
+            if(Carpintero1Timer.Enabled == false)
+            {
+                Carpintero1Timer.Enabled = true;
+                Carpintero2Timer.Enabled = true;
+                Carpintero3Timer.Enabled = true;
+                Carpintero4Timer.Enabled = true;
+                Carpintero5Timer.Enabled = true;
+            }
         }
 
-        private void CronometroTimer_Tick(object sender, EventArgs e)
+        public void SecarMarcosEnAlmacen()
         {
-            minuto++;
-
-            if (minuto == 60)
+            if(Hora == 23 && Minuto == 58)
             {
-                hora++;
-                minuto = 0;
+                if (ColaDeAlmacen.Count != 0)
+                {
+                    foreach (var item in ColaDeAlmacen)
+                    {
+                        ColaPintado.Enqueue(ColaDeAlmacen.Dequeue());
+                    }
+                }
             }
-
-            if (hora == 24)
-            {
-                hora = 0;
-            }
-
-            if (hora > 9 && minuto > 9)
-                TiempoLabel.Text = hora + ":" + minuto;
-
-            if (hora > 9 && minuto < 9)
-                TiempoLabel.Text = hora + ":0" + minuto;
-
-            if (minuto > 9 && (hora >= 0 && hora <= 9))
-                TiempoLabel.Text = "0" + hora + ":" + minuto;
-
-            if ((minuto >= 0 && minuto <= 9) && (hora >= 0 && hora <= 9))
-                TiempoLabel.Text = "0" + hora + ":" + "0" + minuto;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        public void PintarMarcos()
+        {  
+            if (ColaPintado.Count != 0)
+            {
+                int SuperarInspeccion = aleatorio.Next(1, 11);
+
+                if (MarcoPintado == false)
+                {
+                    InicioPintado = Minuto;
+                    DuracionPintado = aleatorio.Next(10, 21);
+                    TerminarPintado = InicioPintado + DuracionPintado;
+
+                    MarcoPintado = true;
+                }
+
+                if (SuperarInspeccion <= 9)
+                {
+                    if (TerminarPintado > 60)
+                        TerminarPintado -= 60;
+
+                    if (TerminarPintado == Minuto)//Si se termino de pintar
+                    {
+                        MarcoPintado = false;
+                        ColaEmpaque.Enqueue(ColaPintado.Dequeue());
+                        TerminarPintado = 0;
+                        MarcosProcesados++;
+                    }
+                }
+                else
+                    ColaDeAlmacen.Enqueue(ColaPintado.Dequeue());
+
+                if (MarcosProcesados == 20)
+                {
+                    PintarTimer.Enabled = false;
+                    int aux = aleatorio.Next(1, 4);
+
+                    if (aux == 1)
+                    {
+                        InicioMantenimiento = Minuto;
+                        TerminarMantenimiento = InicioMantenimiento + 30;
+                    }
+                    else if (aux == 2)
+                    {
+                        InicioMantenimiento = Minuto;
+                        TerminarMantenimiento = InicioMantenimiento + 45;
+                    }
+                    else if (aux == 3)
+                    {
+                        InicioMantenimiento = Minuto;
+                        TerminarMantenimiento = InicioMantenimiento + 60;
+                    }
+                    MarcosProcesados = 0;
+                }
+
+                if (TerminarMantenimiento > 60)
+                    TerminarMantenimiento -= 60;
+
+                if (TerminarMantenimiento == Minuto)
+                {
+                    PintarTimer.Enabled = true;
+                    TerminarMantenimiento = 0;
+                }
+            }
+        }
+
+        public void EmpacarMarcos()
+        {
+            if (ColaEmpaque.Count != 0)
+            {
+                if (MarcoEmpacado == false)
+                {
+                    InicioEmpaque = Minuto;
+                    DuracionEmpaque = aleatorio.Next(10, 16);
+                    TerminarEmpaque = InicioEmpaque + DuracionEmpaque;
+
+                    MarcoEmpacado = true;
+                }
+
+                if (TerminarEmpaque > 60)
+                    TerminarEmpaque -= 60;
+
+                if (TerminarEmpaque == Minuto)//Si se termino de pintar
+                {
+                    MarcoEmpacado = false;
+                    ColaEmpaque.Dequeue();
+                    TerminarEmpaque = 0;
+                }
+            }
+        }
+
+        private void IniciarButton_Click(object sender, EventArgs e)
         {
             CronometroTimer.Enabled = !CronometroTimer.Enabled;
             LlegadaTimer.Enabled = !LlegadaTimer.Enabled;
+            PintarTimer.Enabled = !PintarTimer.Enabled;
+            ProcesosTimer.Enabled = !ProcesosTimer.Enabled;
+            Carpintero1Timer.Enabled = !Carpintero1Timer.Enabled;
+            Carpintero2Timer.Enabled = !Carpintero2Timer.Enabled;
+            Carpintero3Timer.Enabled = !Carpintero3Timer.Enabled;
+            Carpintero4Timer.Enabled = !Carpintero4Timer.Enabled;
+            Carpintero5Timer.Enabled = !Carpintero5Timer.Enabled;
         }
 
-        private void Carpintero1Timer_Tick(object sender, EventArgs e)
+        /*----------------------------------- TIMERS -----------------------------------*/
+        private void CronometroTimer_Tick(object sender, EventArgs e)
         {
-            if(Carpintero1Disponible == true)
+            Minuto++;
+
+            if (Minuto == 60)
             {
-                ColaDeEnsamblaje.Dequeue();
+                Hora++;
+                Minuto = 0;
             }
 
-            if (TiempoCarpintero1 == 2)
+            if (Hora == 24)
             {
+                Dia++;
+                Hora = 0;
+            }
 
+            if (Hora > 9 && Minuto > 9)
+                TiempoLabel.Text = Hora + ":" + Minuto;
+
+            if (Hora > 9 && Minuto < 9)
+                TiempoLabel.Text = Hora + ":0" + Minuto;
+
+            if (Minuto > 9 && (Hora >= 0 && Hora <= 9))
+                TiempoLabel.Text = "0" + Hora + ":" + Minuto;
+
+            if ((Minuto >= 0 && Minuto <= 9) && (Hora >= 0 && Hora <= 9))
+                TiempoLabel.Text = "0" + Hora + ":" + "0" + Minuto;
+
+            if (Dia == 417)//Termina el programa
+            {
+                CronometroTimer.Enabled = false;
+                LlegadaTimer.Enabled = false;
+                ProcesosTimer.Enabled = false;
+                PintarTimer.Enabled = false;
+                Carpintero1Timer.Enabled = false;
+                Carpintero2Timer.Enabled = false;
+                Carpintero3Timer.Enabled = false;
+                Carpintero4Timer.Enabled = false;
+                Carpintero5Timer.Enabled = false;
             }
         }
 
@@ -133,24 +280,230 @@ namespace SimuladorTallerCuadros
             DeterminarCantidadMarcos();
         }
 
+        private void PintarTimer_Tick(object sender, EventArgs e)
+        {
+            PintarMarcos();
+        }
+
+        private void ProcesosTimer_Tick(object sender, EventArgs e)
+        {
+            SecarMarcosEnAlmacen();
+            EmpacarMarcos();
+        }
+
+        private void Carpintero1Timer_Tick(object sender, EventArgs e)
+        {
+            if(ColaDeEnsamblaje.Count != 0)
+            {
+                if (Carpintero1Disponible == true)
+                {
+                    HoraInicioC1 = Hora;
+                    ColaDeAlmacen.Enqueue(ColaDeEnsamblaje.Dequeue());
+                    Carpintero1Disponible = false;
+                }
+
+                if (TiempoCarpintero1 == 2)
+                {
+                    if ((HoraInicioC1 + 2) == Hora)
+                        Carpintero1Disponible = true;
+                }
+
+                if (TiempoCarpintero1 == 3)
+                {
+                    if ((HoraInicioC1 + 3) == Hora)
+                        Carpintero1Disponible = true;
+                }
+
+                if (TiempoCarpintero1 == 4)
+                {
+                    if ((HoraInicioC1 + 4) == Hora)
+                        Carpintero1Disponible = true;
+                }
+
+                if (TiempoCarpintero1 == 5)
+                {
+                    if ((HoraInicioC1 + 5) == Hora)
+                        Carpintero1Disponible = true;
+                }
+
+                if (TiempoCarpintero1 == 6)
+                {
+                    if ((HoraInicioC1 + 6) == Hora)
+                        Carpintero1Disponible = true;
+                }
+            }
+        }
+
         private void Carpintero2Timer_Tick(object sender, EventArgs e)
         {
+            if(ColaDeEnsamblaje.Count != 0)
+            {
+                if (Carpintero2Disponible == true)
+                {
+                    HoraInicioC2 = Hora;
+                    ColaDeAlmacen.Enqueue(ColaDeEnsamblaje.Dequeue());
+                    Carpintero2Disponible = false;
+                }
 
+                if (TiempoCarpintero2 == 2)
+                {
+                    if ((HoraInicioC2 + 2) == Hora)
+                        Carpintero1Disponible = true;
+                }
+
+                if (TiempoCarpintero2 == 3)
+                {
+                    if ((HoraInicioC2 + 3) == Hora)
+                        Carpintero1Disponible = true;
+                }
+
+                if (TiempoCarpintero2 == 4)
+                {
+                    if ((HoraInicioC2 + 4) == Hora)
+                        Carpintero1Disponible = true;
+                }
+
+                if (TiempoCarpintero2 == 5)
+                {
+                    if ((HoraInicioC2 + 5) == Hora)
+                        Carpintero1Disponible = true;
+                }
+
+                if (TiempoCarpintero2 == 6)
+                {
+                    if ((HoraInicioC2 + 6) == Hora)
+                        Carpintero1Disponible = true;
+                }
+            }
         }
 
         private void Carpintero3Timer_Tick(object sender, EventArgs e)
         {
+            if (ColaDeEnsamblaje.Count != 0)
+            {
+                if (Carpintero3Disponible == true)
+                {
+                    HoraInicioC3 = Hora;
+                    ColaDeAlmacen.Enqueue(ColaDeEnsamblaje.Dequeue());
+                    Carpintero3Disponible = false;
+                }
 
+                if (TiempoCarpintero3 == 2)
+                {
+                    if ((HoraInicioC3 + 2) == Hora)
+                        Carpintero1Disponible = true;
+                }
+
+                if (TiempoCarpintero3 == 3)
+                {
+                    if ((HoraInicioC3 + 3) == Hora)
+                        Carpintero1Disponible = true;
+                }
+
+                if (TiempoCarpintero3 == 4)
+                {
+                    if ((HoraInicioC3 + 4) == Hora)
+                        Carpintero1Disponible = true;
+                }
+
+                if (TiempoCarpintero3 == 5)
+                {
+                    if ((HoraInicioC3 + 5) == Hora)
+                        Carpintero1Disponible = true;
+                }
+
+                if (TiempoCarpintero3 == 6)
+                {
+                    if ((HoraInicioC3 + 6) == Hora)
+                        Carpintero1Disponible = true;
+                }
+            }
         }
 
         private void Carpintero4Timer_Tick(object sender, EventArgs e)
         {
+            if (ColaDeEnsamblaje.Count != 0)
+            {
+                if (Carpintero4Disponible == true)
+                {
+                    HoraInicioC4 = Hora;
+                    ColaDeAlmacen.Enqueue(ColaDeEnsamblaje.Dequeue());
+                    Carpintero4Disponible = false;
+                }
 
+                if (TiempoCarpintero4 == 2)
+                {
+                    if ((HoraInicioC4 + 2) == Hora)
+                        Carpintero1Disponible = true;
+                }
+
+                if (TiempoCarpintero4 == 3)
+                {
+                    if ((HoraInicioC4 + 3) == Hora)
+                        Carpintero1Disponible = true;
+                }
+
+                if (TiempoCarpintero4 == 4)
+                {
+                    if ((HoraInicioC4 + 4) == Hora)
+                        Carpintero1Disponible = true;
+                }
+
+                if (TiempoCarpintero4 == 5)
+                {
+                    if ((HoraInicioC4 + 5) == Hora)
+                        Carpintero1Disponible = true;
+                }
+
+                if (TiempoCarpintero4 == 6)
+                {
+                    if ((HoraInicioC4 + 6) == Hora)
+                        Carpintero1Disponible = true;
+                }
+            }
         }
 
         private void Carpintero5Timer_Tick(object sender, EventArgs e)
         {
+            if (ColaDeEnsamblaje.Count != 0)
+            {
+                if (Carpintero5Disponible == true)
+                {
+                    HoraInicioC5 = Hora;
+                    ColaDeAlmacen.Enqueue(ColaDeEnsamblaje.Dequeue());
+                    Carpintero5Disponible = false;
+                }
 
+                if (TiempoCarpintero5 == 2)
+                {
+                    if ((HoraInicioC5 + 2) == Hora)
+                        Carpintero1Disponible = true;
+                }
+
+                if (TiempoCarpintero5 == 3)
+                {
+                    if ((HoraInicioC5 + 3) == Hora)
+                        Carpintero1Disponible = true;
+                }
+
+                if (TiempoCarpintero5 == 4)
+                {
+                    if ((HoraInicioC5 + 4) == Hora)
+                        Carpintero1Disponible = true;
+                }
+
+                if (TiempoCarpintero5 == 5)
+                {
+                    if ((HoraInicioC5 + 5) == Hora)
+                        Carpintero1Disponible = true;
+                }
+
+                if (TiempoCarpintero5 == 6)
+                {
+                    if ((HoraInicioC5 + 6) == Hora)
+                        Carpintero1Disponible = true;
+                }
+            }
         }
 
     }
